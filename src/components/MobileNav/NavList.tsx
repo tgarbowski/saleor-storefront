53c1272e4a4245a6d9de29d90a0728c87d4df96e
michalzietek <michalzietkowski@gmail.com>
2021-04-06 09:44:36 +0200

    changed storefront branding to salingo look alike

--- src/components/MobileNav/NavList.tsx
+++ src/components/MobileNav/NavList.tsx
@@ -10,7 +10,7 @@ import { baseUrl } from "../../app/routes";
 import NavItem, { INavItem } from "./NavItem";
 
 import backImg from "../../images/arrow-back.svg";
-import logoImg from "../../images/logo.svg";
+import logoImg from "images/salingo.svg";
 
 interface NavListProps {
   items: INavItem[];
