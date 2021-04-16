71afd2c488707c30524cc6fd6ea280d6db5fb000
michalzietek <michalzietkowski@gmail.com>
2021-04-16 16:42:43 +0200

    added photo alt display and deleted graphql link

--- src/components/Footer/Nav.tsx
+++ src/components/Footer/Nav.tsx
@@ -17,13 +17,6 @@ class Nav extends React.PureComponent {
                   <h4 className="footer-nav__section-header">
                     <NavLink item={item} />
                   </h4>
-                  <div className="footer-nav__section-content">
-                    {item.children.map(subItem => (
-                      <p key={subItem.id}>
-                        <NavLink item={subItem} />
-                      </p>
-                    ))}
-                  </div>
                 </div>
               ));
             }}
