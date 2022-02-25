464878ef32f48763dd29903d6f2c22ec6dc84183
hubert <igorswatowski@gmail.com>
2022-02-25 15:43:52 +0100

    Changed logo in F4U

--- src/@next/globalStyles/Logo.ts
+++ src/@next/globalStyles/Logo.ts
@@ -1,8 +1,8 @@
 import { shopName } from "@temp/constants";
 
 import C4ULogo from "../../images/c4u_logo.svg";
-import F4ULogo from "../../images/Fashion4youLogo.svg";
 import sailingDefaultLogo from "../../images/logo.svg";
+import F4ULogo from "../../images/logo-f4u.svg";
 
 const Logo =
   shopName === "CLOTHES4U"
--- src/@next/globalStyles/LogoSmall.ts
+++ src/@next/globalStyles/LogoSmall.ts
@@ -1,7 +1,7 @@
 import { shopName } from "@temp/constants";
 
 import C4ULogoSmall from "../../images/c4u_logo_small.svg";
-import F4ULogoSmall from "../../images/Fashion4youLogoSmall.svg";
+import F4ULogoSmall from "../../images/logo-f4u.svg";
 import sailingDefaultLogoSmall from "../../images/logo-small.svg";
 
 const LogoSmall =
