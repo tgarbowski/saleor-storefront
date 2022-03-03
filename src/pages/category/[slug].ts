cd947c9f3db01134254e7ec158ddc81424c20bd5
Tooomas <tomek.dominiczak@gmail.com>
2022-03-03 14:00:59 +0100

    Change products and categories from SSG to SSR

--- src/pages/category/[slug].ts
+++ src/pages/category/[slug].ts
@@ -1,13 +1,10 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import "@temp/constants";
 
-import {
-  incrementalStaticRegenerationRevalidate,
-  staticPathsFallback,
-  staticPathsFetchBatch,
-} from "@temp/constants";
+import { GetServerSideProps } from "next";
+
 import { CategoryView, CategoryViewProps } from "@temp/views/Category";
 import {
-  exhaustList,
+  // exhaustList,
   getFeaturedProducts,
   getSaleorApi,
   getShopAttributes,
@@ -15,23 +12,7 @@ import {
 
 export default CategoryView;
 
-export const getStaticPaths: GetStaticPaths<CategoryViewProps["params"]> =
-  async () => {
-    const { api } = await getSaleorApi();
-    const { data } = await exhaustList(
-      api.categories.getList({
-        first: staticPathsFetchBatch,
-      })
-    );
-
-    const paths = data.map(({ slug }) => ({
-      params: { slug },
-    }));
-
-    return { paths, fallback: staticPathsFallback };
-  };
-
-export const getStaticProps: GetStaticProps<
+export const getServerSideProps: GetServerSideProps<
   CategoryViewProps,
   CategoryViewProps["params"]
 > = async ({ params: { slug } }) => {
@@ -57,10 +38,76 @@ export const getStaticProps: GetStaticProps<
   }
 
   return {
-    revalidate: incrementalStaticRegenerationRevalidate,
     props: {
       data,
       params: { slug },
     },
   };
 };
+
+// import { GetStaticPaths, GetStaticProps } from "next";
+
+// import {
+//   incrementalStaticRegenerationRevalidate,
+//   staticPathsFallback,
+//   staticPathsFetchBatch,
+// } from "@temp/constants";
+// import { CategoryView, CategoryViewProps } from "@temp/views/Category";
+// import {
+//   exhaustList,
+//   getFeaturedProducts,
+//   getSaleorApi,
+//   getShopAttributes,
+// } from "@utils/ssr";
+
+// export default CategoryView;
+
+// export const getStaticPaths: GetStaticPaths<CategoryViewProps["params"]> =
+//   async () => {
+//     const { api } = await getSaleorApi();
+//     const { data } = await exhaustList(
+//       api.categories.getList({
+//         first: staticPathsFetchBatch,
+//       })
+//     );
+
+//     const paths = data.map(({ slug }) => ({
+//       params: { slug },
+//     }));
+
+//     return { paths, fallback: staticPathsFallback };
+//   };
+
+// export const getStaticProps: GetStaticProps<
+//   CategoryViewProps,
+//   CategoryViewProps["params"]
+// > = async ({ params: { slug } }) => {
+//   let data = null;
+//   const { api } = await getSaleorApi();
+//   const { data: details } = await api.categories.getDetails({ slug });
+
+//   if (details) {
+//     const { id } = details;
+
+//     const [attributes, featuredProducts, ancestors] = await Promise.all([
+//       getShopAttributes({ categoryId: id }),
+//       getFeaturedProducts(),
+//       api.categories.getAncestors({ first: 5, id }).then(({ data }) => data),
+//     ]);
+//     data = {
+//       details,
+//       ancestors,
+//       featuredProducts,
+//       attributes,
+//       id,
+//     };
+//   }
+
+//   return {
+//     revalidate: incrementalStaticRegenerationRevalidate,
+//     props: {
+//       data,
+//       params: { slug },
+//     },
+//   };
+// };
