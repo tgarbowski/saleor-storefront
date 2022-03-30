59c3eb60a2b22331dd262ff7d3085f04cb7f7893
hubert <igorswatowski@gmail.com>
2022-03-30 10:42:55 +0200

    Fixed DefaultHeroImg

--- src/@next/globalStyles/DefaultHeroImg.ts
+++ src/@next/globalStyles/DefaultHeroImg.ts
@@ -1,16 +1,8 @@
 import { shopName } from "@temp/constants";
 
 import DefaultHeroWomanImg from "../../images/hero-img.jpg";
-import DefaultHeroKidsImg from "../../images/hero-kids-img.jpg";
-import DefaultHeroManImg from "../../images/hero-man-img.jpg";
 
 const DefaultHero =
-  shopName === "SALINGOKIDS"
-    ? DefaultHeroKidsImg
-    : shopName === "SALINGOMAN"
-    ? DefaultHeroManImg
-    : shopName === "SALINGOWOMAN"
-    ? DefaultHeroWomanImg
-    : DefaultHeroWomanImg;
+  shopName === "FASHION4YOU" ? DefaultHeroWomanImg : DefaultHeroWomanImg;
 
 export default DefaultHero;
