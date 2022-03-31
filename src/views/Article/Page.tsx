92995cd9082d14014550ffc1722e6205e8df2901
hubert <igorswatowski@gmail.com>
2022-03-31 13:04:54 +0200

    Changed footer logo on default theme, fixed article view, added animation to footer logo, added condition when hasProducts not exist in category

--- src/views/Article/Page.tsx
+++ src/views/Article/Page.tsx
@@ -5,22 +5,14 @@ import { Breadcrumb, Breadcrumbs } from "../../components";
 
 interface PageProps {
   breadcrumbs: Breadcrumb[];
-  headerImage: string | null;
   page: {
     content: any | null;
     title: string;
   };
 }
-export const Page: React.FC<PageProps> = ({
-  breadcrumbs,
-  headerImage,
-  page,
-}) => (
+export const Page: React.FC<PageProps> = ({ breadcrumbs, page }) => (
   <div className="article-page">
-    <div
-      className="article-page__header"
-      style={headerImage ? { backgroundImage: `url(${headerImage})` } : null}
-    >
+    <div className="article-page__header">
       <span className="article-page__header__title">
         <h1>{page.title}</h1>
       </span>
