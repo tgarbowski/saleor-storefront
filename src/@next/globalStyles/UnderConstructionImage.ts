7ea631908771ce80d80518801b30ae2193780134
Igor Swatowski <igi66@wp.pl>
2022-05-26 09:03:48 +0200

    Added new construction page (#77)

--- src/@next/globalStyles/UnderConstructionImage.ts
+++ src/@next/globalStyles/UnderConstructionImage.ts
@@ -1,8 +1,22 @@
 import { shopName } from "@temp/constants";
 
-import UnderConstructionImage from "../../images/UnderConstructionImage.svg";
+import CloudMainImg from "../../images/cloud-main.svg";
+import CloudMainSmallImgFirst from "../../images/cloud-small-first.svg";
+import CloudMainSmallImgSecond from "../../images/cloud-small-second.svg";
+import F4UUnderConstructionLogo from "../../images/F4UUnderConstructionLogo.svg";
+import FacebookSmallIcon from "../../images/facebook-small-icon.svg";
+import UnderConstructionImg from "../../images/first-bg.png";
+import InstagramSmallIcon from "../../images/instagram-small-icon.svg";
 
-const UnderConstructionImg =
-  shopName === "FASHION4YOU" ? UnderConstructionImage : null;
 
-export default UnderConstructionImg;
+const UnderConstructionImage =
+  shopName === "FASHION4YOU" ? UnderConstructionImg : null;
+
+export const CloudMainImage = shopName === "FASHION4YOU" ? CloudMainImg : null;
+export const CloudMainSmallImageFirst = shopName  === "FASHION4YOU" ? CloudMainSmallImgFirst : null;
+export const CloudMainSmallImageSecond = shopName  === "FASHION4YOU" ? CloudMainSmallImgSecond : null;
+export const InstagramSmallIcn = shopName  === "FASHION4YOU" ? InstagramSmallIcon : null;
+export const FacebookSmallIcn = shopName  === "FASHION4YOU" ? FacebookSmallIcon : null;
+export const F4UUnderConstructionLogoImage = shopName  === "FASHION4YOU" ? F4UUnderConstructionLogo : null;
+
+export default UnderConstructionImage;
