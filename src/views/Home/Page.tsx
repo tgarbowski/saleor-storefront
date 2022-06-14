16584f632af5d49470d57e41f261aea23300fa75
IgorSwatowski <igorswatowski@gmail.com>
2022-06-14 12:45:21 +0200

    Added zoom on productImage, hide one category for C4U branding

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -1,20 +1,22 @@
-import Link from 'next/link';
-import * as React from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
-import { generatePath } from 'react-router';
+import Link from "next/link";
+import * as React from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { generatePath } from "react-router";
 
-import { paths } from '@paths';
-import DefaultHero from '@styles/DefaultHeroImg';
-import { FooterUsp } from '@temp/components/FooterUsp/FooterUsp';
-import { shopName } from '@temp/constants';
-import { FeaturedProducts } from '@utils/ssr';
+import { paths } from "@paths";
+import DefaultHero from "@styles/DefaultHeroImg";
+import { FooterUsp } from "@temp/components/FooterUsp/FooterUsp";
+import { shopName } from "@temp/constants";
+import { FeaturedProducts } from "@utils/ssr";
 
-import { Button, ProductsFeatured } from '../../components';
-import { structuredData } from '../../core/SEO/Homepage/structuredData';
-import { HomePageProducts_categories, HomePageProducts_shop } from './gqlTypes/HomePageProducts';
+import { Button, ProductsFeatured } from "../../components";
+import { structuredData } from "../../core/SEO/Homepage/structuredData";
+import {
+  HomePageProducts_categories,
+  HomePageProducts_shop,
+} from "./gqlTypes/HomePageProducts";
 
-import './scss/index.scss';
-
+import "./scss/index.scss";
 
 const Page: React.FC<{
   categories: HomePageProducts_categories;
@@ -30,6 +32,7 @@ const Page: React.FC<{
     shopName === "FASHION4YOU"
       ? categories.edges.slice(0, -2)
       : categories.edges;
+
   return (
     <>
       <script className="structured-data-list" type="application/ld+json">
@@ -100,7 +103,8 @@ const Page: React.FC<{
             </h2>
             <div className="home-page__categories__list">
               {visibleCategory.map(({ node: category }) => {
-                return (
+                return shopName === "CLOTHES4U" &&
+                  category.name === "Detal" ? null : (
                   <div key={category.id} className="home-page__category-item">
                     <Link
                       href={generatePath(paths.category, {
