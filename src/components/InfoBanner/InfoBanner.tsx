bc877280425cc18ee9169ec92b2994aa01808db7
IgorSwatowski <igorswatowski@gmail.com>
2022-06-28 15:01:14 +0200

    Added promo info

---
+++ src/components/InfoBanner/InfoBanner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./scss/index.scss";
+
+export const InfoBanner = () => {
+  return (
+    <section className="marketing-bar-global">
+      <div className="marketing-bar-content">
+        <div className="marketing-bar-content-description">
+          CODZIENNIE NOWA DOSTAWA! SPRAWDŹ TERAZ NASZĄ OFERTĘ
+        </div>
+      </div>
+    </section>
+  );
+};
