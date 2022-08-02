7483293f7ca0eb3bb7b79412b294d4be332117ef
IgorSwatowski <igorswatowski@gmail.com>
2022-08-02 13:23:01 +0200

    Added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable lines-between-class-members */
+/* eslint-disable react/no-danger */
+import DOMPurify from "dompurify";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
 
@@ -7,10 +10,29 @@ class MyDocument extends Document {
     return { ...initialProps };
   }
 
+  data = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+  });
+`;
+
+  sanitizedData = {
+    __html: DOMPurify.sanitize(this.data),
+  };
+
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script dangerouslySetInnerHTML={this.sanitizedData} />
+        </Head>
         <body>
           <Main />
           <NextScript />
