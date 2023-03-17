30cf0da567c6929fa963b75ba3c8c522a81392cf
IgorSwatowski <igi66@wp.pl>
2023-03-17 15:39:20 +0100

    Add accept terms checkbox

--- src/@next/components/organisms/CheckoutReview/stories.tsx
+++ src/@next/components/organisms/CheckoutReview/stories.tsx
@@ -5,4 +5,12 @@ import { CheckoutReview } from ".";
 
 storiesOf("@components/organisms/CheckoutReview", module)
   .addParameters({ component: CheckoutReview })
-  .add("default", () => <CheckoutReview />);
+  .add("default", () => (
+    <CheckoutReview
+      // eslint-disable-next-line react/jsx-no-bind, func-names
+      handleTermsChange={function (terms: boolean): void {
+        throw new Error("Function not implemented.");
+      }}
+      pages={undefined}
+    />
+  ));
